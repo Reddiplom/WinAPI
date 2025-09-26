d31073802dd870be785bee5812db7dda917880e9
xboxl <xboxl@DANIIL>
2025-09-27 00:42:42 +0300

    Login DONE

--- WinAPI/main.cpp
+++ WinAPI/main.cpp
@@ -24,14 +24,48 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_INITDIALOG:
 	{
 		HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
-		SetFocus(hEditLogin);
+		SetWindowText(hEditLogin, "Введите имя пользователя");
+
+
+		//SetFocus(hEditLogin);
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
 	}
-		break;
+	break;
 	case WM_COMMAND:
+	{
+		HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+
+		if (LOWORD(wParam) == IDC_EDIT_LOGIN) 
+		{
+			
+			if (HIWORD(wParam) == EN_SETFOCUS)
+			{
+				char text[256];
+				GetWindowText(hEditLogin, (LPSTR)text, 256);
+
+				
+				if (strcmp(text, "Введите имя пользователя") == 0)
+				{
+					SetWindowText(hEditLogin, "");
+				}
+			}
+			
+			else if (HIWORD(wParam) == EN_KILLFOCUS)
+			{
+				char text[256];
+				GetWindowText(hEditLogin, (LPSTR)text, 256);
+
+				
+				if (strlen(text) == 0)
+				{
+					SetWindowText(hEditLogin, "Введите имя пользователя");
+				}
+			}
+		}
 		switch (LOWORD(wParam))
 		{
+
 		case IDC_BUTTON_COPY:
 		{
 			const int SIZE = 256;
@@ -42,14 +76,23 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
 		}
 		break;
-		case IDOK:
-			MessageBox(hwnd, "Была нажата кнопка ОК", "Info", MB_OK | MB_ICONINFORMATION); break;
+		case IDOK: MessageBox(hwnd, "Была нажата кнопка ОК", "Info", MB_OK | MB_ICONINFORMATION); break;
 		case IDCANCEL: EndDialog(hwnd, 0);break;
+
+
 		}
-		break;
-	case WM_CLOSE:
-		EndDialog(hwnd, 0);
-		break;
+
+
+	}
+	break;
+		case WM_CLOSE:
+			EndDialog(hwnd, 0);
+			break;
+
+			
+
+		
+		
 	}
 	return FALSE;
 }
